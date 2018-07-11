d5373b9f523f4ef31066ef143e735cdc90752536
Casey Duncan <caseyduncan@Casey-Duncan.local>
2018-07-11 17:20:22 -0600

    Add .gitconfig and oh-my-zsh custom configs

---
+++ .oh-my-zsh/custom/aliases.zsh
@@ -0,0 +1,18 @@
+# Casey's Aliases
+
+alias -g L='| less'
+alias -g G='| grep'
+alias -g T='| tee'
+
+alias x='chmod +x'
+alias ll='ls -lh'
+alias la='ls -AF'
+alias ls='ls -F'
+
+alias pd='popd >/dev/null'
+alias psg='ps awx | grep'
+alias grep='egrep'
+alias rgrep='egrep -r'
+alias top='top -c'
+alias tf='tail -F'
+alias h='history'
---
+++ .oh-my-zsh/custom/powerlevel9k.zsh
@@ -0,0 +1,4 @@
+# Custom Powerlevel 9k config
+
+POWERLEVEL9K_COLOR_SCHEME='light'
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(dir rbenv vcs)
