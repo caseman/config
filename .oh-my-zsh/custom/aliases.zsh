0f3ce26ce47a243cadb5aacde5bfb4c2b1b7348d
Casey Duncan <casey.duncan@gmail.com>
2018-07-17 18:27:10 -0700

    Add global aliases for stderr redirects and piping to vim

--- .oh-my-zsh/custom/aliases.zsh
+++ .oh-my-zsh/custom/aliases.zsh
@@ -3,7 +3,13 @@
 alias -g L='| less'
 alias -g LL='2>&1 | less'
 alias -g G='| grep'
+alias -g GG='2>&1 | grep'
 alias -g T='| tee'
+alias -g TT='2>&1 | tee'
+alias -g V='| vim -'
+alias -g VV='2>&1 | vim -'
+alias -g HD='| head'
+alias -g TL='| tail'
 alias -g NUL='> /dev/null 2>&1'
 
 alias x='chmod +x'
