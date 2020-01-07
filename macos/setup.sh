97d35969d39e8105e09796fb7e95815465ea9eb5
Casey Duncan <casey.duncan@coinbase.com>
2020-01-07 12:12:29 -0700

    remove syntastic

--- macos/setup.sh
+++ macos/setup.sh
@@ -76,7 +76,6 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 git clone https://github.com/tpope/vim-sensible.git ~/.vim/bundle/vim-sensible
 git clone https://tpope.io/vim/unimpaired.git ~/.vim/bundle/unimpaired
 git clone https://github.com/tpope/vim-fugitive.git ~/vim/bundle/vim-fugitive
-git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
 git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
 git clone https://github.com/ctrlpvim/ctrlp.vim.git  ~/.vim/bundle/ctrlp.vim
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
